6bb1983ec414af810f080107b4801f0fd344f6a9
avdelag <149859375+avdelag@users.noreply.github.com>
2026-04-01 11:00:41 -0500

    fix: resolve multiple React instances via resolve.dedupe in vite.config.ts to fix useContext error

--- vite.config.ts
+++ vite.config.ts
@@ -53,6 +53,7 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+    dedupe: ['react', 'react-dom', 'react-router-dom'],
   },
   build: {
     target: 'esnext',
