90d2323b8c20306f000d0bc5697df3b698503c22
avdelag <woorkify@gmail.com>
2026-06-14 14:27:25 -0500

    Fix duplicate geolocation patches breaking postinstall on Vercel

--- scripts/patch-capacitor-xcode14.cjs
+++ scripts/patch-capacitor-xcode14.cjs
@@ -49,47 +49,9 @@ if (fs.existsSync(encoderPath)) {
   }
 }
 
-// Geolocation patches
-const geoErrorPath = path.join(__dirname, '../node_modules/@capacitor/geolocation/ios/Sources/GeolocationPlugin/GeolocationError.swift');
-if (fs.existsSync(geoErrorPath)) {
-  let content = fs.readFileSync(geoErrorPath, 'utf8');
-  let changed = false;
-  if (content.includes('case .positionUnavailable: 2')) {
-    content = content.replace(/case \.positionUnavailable: 2/g, 'case .positionUnavailable: return 2');
-    content = content.replace(/case \.permissionDenied: 3/g, 'case .permissionDenied: return 3');
-    content = content.replace(/case \.locationServicesDisabled: 7/g, 'case .locationServicesDisabled: return 7');
-    content = content.replace(/case \.permissionRestricted: 8/g, 'case .permissionRestricted: return 8');
-    content = content.replace(/case \.getCurrentPosition: 4/g, 'case .getCurrentPosition: return 4');
-    content = content.replace(/case \.watchPosition: 5/g, 'case .watchPosition: return 5');
-    content = content.replace(/case \.clearWatch: 6/g, 'case .clearWatch: return 6');
-    changed = true;
-  }
-  if (content.includes('case .positionUnavailable: "There was')) {
-    content = content.replace(/case \.positionUnavailable: "There was en error trying to obtain the location\."/g, 'case .positionUnavailable: return "There was en error trying to obtain the location."');
-    content = content.replace(/case \.permissionDenied: "Location permission request was denied\."/g, 'case .permissionDenied: return "Location permission request was denied."');
-    content = content.replace(/case \.locationServicesDisabled: "Location services are not enabled\."/g, 'case .locationServicesDisabled: return "Location services are not enabled."');
-    content = content.replace(/case \.permissionRestricted: "Application's use of location services was restricted\."/g, 'case .permissionRestricted: return "Application\'s use of location services was restricted."');
-    content = content.replace(/case \.inputArgumentsIssue\(let target\): "The '\\\\\\(target\\.rawValue\\\\\\)' input parameters aren't valid\\."/g, 'case .inputArgumentsIssue(let target): return "The \'\\\\(target.rawValue)\' input parameters aren\'t valid."');
-    changed = true;
-  }
-  if (changed) {
-    fs.writeFileSync(geoErrorPath, content);
-    console.log('Patched GeolocationError.swift');
-  }
-}
+// Geolocation patches removed because they are handled by patch-package
 
-const geoPluginPath = path.join(__dirname, '../node_modules/@capacitor/geolocation/ios/Sources/GeolocationPlugin/GeolocationPlugin.swift');
-if (fs.existsSync(geoPluginPath)) {
-  let content = fs.readFileSync(geoPluginPath, 'utf8');
-  if (content.includes('let status = switch locationService?.authorisationStatus {')) {
-    content = content.replace(
-      /let status = switch locationService\?\.authorisationStatus \{\n\s*case \.restricted, \.denied: Constants\.AuthorisationStatus\.Status\.denied\n\s*case \.authorisedAlways, \.authorisedWhenInUse: Constants\.AuthorisationStatus\.Status\.granted\n\s*default: Constants\.AuthorisationStatus\.Status\.prompt\n\s*\}/g,
-      'let status: String\n        switch locationService?.authorisationStatus {\n        case .restricted, .denied: status = Constants.AuthorisationStatus.Status.denied\n        case .authorisedAlways, .authorisedWhenInUse: status = Constants.AuthorisationStatus.Status.granted\n        default: status = Constants.AuthorisationStatus.Status.prompt\n        }'
-    );
-    fs.writeFileSync(geoPluginPath, content);
-    console.log('Patched GeolocationPlugin.swift');
-  }
-}
+// Geolocation patch removed because it is now handled by patch-package
 
 // Contacts patches
 const contactsPluginPath = path.join(__dirname, '../node_modules/@capacitor-community/contacts/ios/Sources/ContactsPlugin/ContactsPlugin.swift');
