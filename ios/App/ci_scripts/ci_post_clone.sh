d809eda98b95a0dfb929b0e26d1d61468d09085e
avdelag <woorkify@gmail.com>
2026-07-29 20:02:35 -0500

    fix: remove brew install from ci_post_clone.sh to fix Xcode Cloud script failure

--- ios/App/ci_scripts/ci_post_clone.sh
+++ ios/App/ci_scripts/ci_post_clone.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
-
 echo "Navigating to ios/App directory..."
 cd ..
 
-echo "Installing CocoaPods..."
-export HOMEBREW_NO_AUTO_UPDATE=1
-brew install cocoapods
-
 echo "Running pod install..."
+# Xcode Cloud already has CocoaPods installed, no need to use brew.
 pod install
