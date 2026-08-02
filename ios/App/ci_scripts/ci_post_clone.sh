79e18a43722febb2709d870d6a4205ecff5623c7
Alejandro Villarreal <alejandro@example.com>
2026-08-02 01:23:18 -0500

    Fix Xcode Cloud script"

--- ios/App/ci_scripts/ci_post_clone.sh
+++ ios/App/ci_scripts/ci_post_clone.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+# Export UTF-8 encoding for CocoaPods
+export LANG=en_US.UTF-8
+
 # Install node (Xcode Cloud requires this to ensure npm/npx are available in PATH)
 brew install node
 
@@ -9,6 +12,9 @@ cd ../../../
 
 echo "Installing Node dependencies..."
 npm ci
+
+echo "Building web app..."
+npm run build
 
 echo "Syncing Capacitor plugins and running pod install..."
 npx cap sync ios
